071fbf42025540503e63cd23772dc3a0900094f8
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-18 00:20:44 +0100

    Fix issue with date in update script

--- update.sh
+++ update.sh
@@ -4,5 +4,8 @@ echo "Backing up helix config..."
 cp ~/.config/helix/config.toml ~/.config/helix/languages.toml .
 
 echo "Committing and pushing changes..."
-date_time=$(date +%Y-%m-%d %H:%M:%S)
-git commit -am "Config update $date_time"
+date_time=$(date +"%Y-%m-%d %H:%M:%S")
+commit_message="Config update $date_time"
+echo "Commit message: '$commit_message'"
+git add --all
+git commit -m $commit_message
