8e79bf58b7f81ee1be22e6f65123996e7fa1956a
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-18 01:42:47 +0100

    Add firefox installation script for minimal debian distro

---
+++ install-firefox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script adds the Mozilla apt repo so that firefox can be installed
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+sudo apt update && sudo apt install firefox
