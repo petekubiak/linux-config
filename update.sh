5a9ba0317a4182ce321b88dab08c5735e9ed21d8
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-18 01:05:38 +0100

    Config update@2024-08-18 01:05:38

--- update.sh
+++ update.sh
@@ -4,6 +4,11 @@ echo "Backing up helix config..."
 mkdir -p helix
 cp ~/.config/helix/config.toml ~/.config/helix/languages.toml helix/
 
+echo "Backing up i3 config..."
+mkdir -p i3
+cp ~/.config/i3/config i3/
+sudo cp /etc/i3status.conf i3/
+
 echo "Committing and pushing changes..."
 date_time=$(date +"%Y-%m-%d %H:%M:%S")
 git add --all
