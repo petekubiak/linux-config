75dea9eb58ba2994e4788c691639abee8af4a0a0
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-18 01:24:25 +0100

    Add diff and optional commit message to update script

--- update.sh
+++ update.sh
@@ -12,5 +12,12 @@ sudo cp /etc/i3status.conf i3/
 echo "Committing and pushing changes..."
 date_time=$(date +"%Y-%m-%d@%H:%M:%S")
 git add --all
+git diff --staged
+read -p 'Please enter a commit message: '$commit_message
+if [[ -z $commit_message ]]; then
+  commit_message="Config update $date_time"
+else
+  commit_message="Config update $date_time: $commit_message"
+fi
 git commit -m "Config update $date_time"
 git push
