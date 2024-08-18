fdc45a6c0bf2ecfe09fff885402afc64e291e986
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-18 01:07:03 +0100

    Fix position of @ symbol in commit message

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ cp ~/.config/i3/config i3/
 sudo cp /etc/i3status.conf i3/
 
 echo "Committing and pushing changes..."
-date_time=$(date +"%Y-%m-%d %H:%M:%S")
+date_time=$(date +"%Y-%m-%d@%H:%M:%S")
 git add --all
-git commit -m "Config update@$date_time"
+git commit -m "Config update $date_time"
 git push
